59958f64936a897363e31bf9d4a5c3dac6e16450
Qujuan D Miller <75464655+QDMS@users.noreply.github.com>
2021-08-27 22:37:03 -0400

    Update

--- Assets/Scripts/CurrentPlayer.cs
+++ Assets/Scripts/CurrentPlayer.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CurrentPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string username;
+    public string email;
+    public int score;
 }
--- Assets/Scripts/LoginUser.cs
+++ Assets/Scripts/LoginUser.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class LoginUser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public Button loginButton;
+    public TMP_Text loginButtonText;
+
+    public GameObject currentPlayerObject;
+
+    public void Login()
     {
-        
+        loginButton.interactable = false;
+        if (usernameInput.text.Length < 3)
+        {
+            ErrorOnLoginMessage("Check Username");
+        }
+        else if (passwordInput.text.Length < 3)
+        {
+            ErrorOnLoginMessage("Check Password");
+        }
+        else
+        {
+            StartCoroutine(SendLoginForm());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ErrorOnLoginMessage(string message)
     {
-        
+        loginButton.GetComponent<Image>().color = Color.red;
+        loginButtonText.text = message;
+        loginButtonText.fontSize = 50;
+    }
+    
+    public void ResetLoginButton()
+    {
+        Color g = new Color32(41, 113, 52, 168);
+        loginButton.GetComponent<Image>().color = g;
+        loginButtonText.text = "Login";
+        loginButtonText.fontSize = 75;
+        loginButton.interactable = true;
+    }
+
+    IEnumerator SendLoginForm()
+    {
+        WWWForm LoginInfo = new WWWForm();
+        LoginInfo.AddField("apppassword", "tismhouse");
+        LoginInfo.AddField("username", usernameInput.text);
+        LoginInfo.AddField("password", passwordInput.text);
+        UnityWebRequest loginRequest = UnityWebRequest.Post("http://localhost/cruds/loginuser.php", LoginInfo);
+        yield return loginRequest.SendWebRequest();
+        if (loginRequest.error == null)
+        {
+            Debug.Log("Form Sent");
+        }
+        else
+        {
+            Debug.Log(loginRequest.error);
+            var currentPlayer = Instantiate(currentPlayerObject, new Vector3(0, 0, 0), Quaternion.identity);
+        }
     }
 }
